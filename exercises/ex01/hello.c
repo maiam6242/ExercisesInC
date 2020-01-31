8d84eaba0404d84bac3c696d1f3826348821ca5e
maiam6242 <maiam6242@gmail.com>
2020-01-30 19:10:36 -0500

    Part two

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    int x = 5;
+    printf("Hello World!\n");
+
+    // printf("%i\n", x);
+
+    // int y = x + 1;
+    // printf("%i", y);
     return 0;
 }
+
+/*
+The optimized code is shorter, and aligns memory at the top of the program. 
+It also has steps that are less and are different in the .LFB0 section. The
+optimized code from both questions 3 and 4 is identical since it is functionally
+very similar (aside from the variable values). My biggest errors (or discoveries) from these experiments 
+were using the -O2 and -S tags. I didn't know that they were case sensitive. 
+*/
